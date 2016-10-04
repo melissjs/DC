ed78f334601ed65addad4306fba4083cf63ba503
Eric Hillis <ehillis@jtel-sscsd.local>
2016-10-04 13:22:11 -0700

    added code for popping up selections of presidential support.

--- app/pages/vote/vote.ts
+++ app/pages/vote/vote.ts
@@ -1,33 +1,46 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import {DemographicsPage} from '../demographics/demographics';
-import {LevelofsupportComponent} from '../levelofsupport/levelofsupport';
 
 @Component({
-  templateUrl: 'build/pages/vote/vote.html',
-  directives: [LevelofsupportComponent],
+  templateUrl: 'build/pages/vote/vote.html'
 })
 export class VotePage {
+    presVote: string;
 
-  constructor(private navCtrl: NavController) {
-  this.navCtrl = navCtrl;
-  }
+    constructor(private navCtrl: NavController, private alertCtrl: AlertController) {
+	this.navCtrl = navCtrl;
+	this.presVote = null;
+    }
 
     onSubmit() {
         var that = this;
         try {
-            that.navCtrl.push(DemographicsPage, {
-            });
+	    if (this.presVote == null) {
+		let alert = this.alertCtrl.create({
+		    title: 'No Presidential Vote Selected!',
+		    subTitle: 'Please Make a Selection',
+		    buttons: ['OK']
+		});
+		alert.present();
+	    } else {
+		that.navCtrl.push(DemographicsPage, {
+		});
+	    }
 
         } catch (EE) {
-            console.log('error in Submitting, exc='+ EE.toString())
+	    let alert = this.alertCtrl.create({
+		title: 'error in Submitting',
+		subTitle: EE.toString(),
+		buttons: ['OK']
+            });
+            alert.present();
+	    console.log('error in Submitting, exc='+ EE.toString())
         }
     }
 
-   onFocusLOSGE(yesOrNo){
-       var isFocusedOn = yesOrNo;
-       if (isFocusedOn == "yes"){
-       return "yes";}
+    onChangePresVote(value){
+	this.presVote = value;
    }
 
 }
