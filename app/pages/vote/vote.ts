607480db82e4b9def10fd1a013084ab54622f52e
Eric Hillis <ehillis@jtel-sscsd.local>
2016-10-04 13:24:00 -0700

    forgot to update tabs to spaces.

--- app/pages/vote/vote.ts
+++ app/pages/vote/vote.ts
@@ -9,38 +9,38 @@ export class VotePage {
     presVote: string;
 
     constructor(private navCtrl: NavController, private alertCtrl: AlertController) {
-	this.navCtrl = navCtrl;
-	this.presVote = null;
+        this.navCtrl = navCtrl;
+        this.presVote = null;
     }
 
     onSubmit() {
         var that = this;
         try {
-	    if (this.presVote == null) {
-		let alert = this.alertCtrl.create({
-		    title: 'No Presidential Vote Selected!',
-		    subTitle: 'Please Make a Selection',
-		    buttons: ['OK']
-		});
-		alert.present();
-	    } else {
-		that.navCtrl.push(DemographicsPage, {
-		});
-	    }
+            if (this.presVote == null) {
+                let alert = this.alertCtrl.create({
+                    title: 'No Presidential Vote Selected!',
+                    subTitle: 'Please Make a Selection',
+                    buttons: ['OK']
+                });
+                alert.present();
+            } else {
+                that.navCtrl.push(DemographicsPage, {
+                });
+            }
 
         } catch (EE) {
-	    let alert = this.alertCtrl.create({
-		title: 'error in Submitting',
-		subTitle: EE.toString(),
-		buttons: ['OK']
+            let alert = this.alertCtrl.create({
+                title: 'error in Submitting',
+                subTitle: EE.toString(),
+                buttons: ['OK']
             });
             alert.present();
-	    console.log('error in Submitting, exc='+ EE.toString())
+            console.log('error in Submitting, exc='+ EE.toString())
         }
     }
 
     onChangePresVote(value){
-	this.presVote = value;
+        this.presVote = value;
    }
 
 }
