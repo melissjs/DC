3fd6b81684dbf6ce2cb08ed7f5b67b3a3b8d181c
Melissa Schwartz <melissjs@gmail.com>
2016-10-06 03:23:34 -0700

    amendment record

--- app/pages/amendmentrecord/amendmentrecord.ts
+++ app/pages/amendmentrecord/amendmentrecord.ts
@@ -1,19 +1,54 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import {VoterecordPage} from '../voterecord/voterecord';
 
-/*
-  Generated class for the AmendmentrecordPage page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+
+
 @Component({
   templateUrl: 'build/pages/amendmentrecord/amendmentrecord.html',
 })
 export class AmendmentrecordPage {
+incorrectSelection: string;
+correctSelection: string;
+passcode: string;
 
-  constructor(private navCtrl: NavController) {
 
+  constructor(private navCtrl: NavController, private alertCtrl: AlertController) {
+    this.navCtrl = navCtrl;
+    this.incorrectSelection = null;
   }
 
+//
+ onSubmit() {
+        var that = this;
+        var passcode = null;
+        try {
+            if (this.passcode == null) {
+                let alert = this.alertCtrl.create({
+                    title: 'Team lead passcode required.',
+                    subTitle: 'Amendment records require a team lead passcode for verification; please ask one of your team leads to enter their code.',
+                    buttons: ['OK']
+                });
+                alert.present();
+            } else {
+                that.navCtrl.push(VoterecordPage, {
+                });
+            }
+
+        } catch (EE) {
+            let alert = this.alertCtrl.create({
+                title: 'error in Submitting',
+                subTitle: EE.toString(),
+                buttons: ['OK']
+            });
+            alert.present();
+            console.log('error in Submitting, exc='+ EE.toString())
+        }
+    }
+//
 }
+
+ 
+
+   
